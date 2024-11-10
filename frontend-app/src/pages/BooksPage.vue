25835b191b93ac49bec8b9f3917901135fa95650
Denis Ponyakov <nexons.97@gmail.com>
2024-11-10 23:18:37 +0300

    Added books listing using axios api request

--- frontend-app/src/pages/BooksPage.vue
+++ frontend-app/src/pages/BooksPage.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { listBooks, BookResponseDTO } from '@/entities/book'
 
+const books = ref<BookResponseDTO[]>([]);
+
+listBooks()
+	.then((_books) => {
+		books.value = _books
+	});
 </script>
 
 <template>
 	<div>
 		<h1>Books Page</h1>
+		
+		<div class="book-entry" v-for="book in books">
+			{{ book }}
+		</div>
 		
 		<a-button type="primary">Add Book</a-button>
 	</div>
