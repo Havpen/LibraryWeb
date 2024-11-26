972e405e997f4a8156f6c30914d399cfa71e2cd8
Havpen <zhenya_alisievich@mail.ru>
2024-11-26 21:37:02 +0300

    Added JavaFX

--- backend-app/src/main/java/com/alisievich/LibraryManagementApp.java
+++ backend-app/src/main/java/com/alisievich/LibraryManagementApp.java
@@ -2,12 +2,11 @@ package com.alisievich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class LibraryManagementApp {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementApp.class, args);
 	}
 }
+
---
+++ backend-app/src/main/java/com/alisievich/MainController.java
@@ -0,0 +1,9 @@
+package com.alisievich;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainController {
+    // Добавьте методы для обработки событий (если нужно)
+}
+
